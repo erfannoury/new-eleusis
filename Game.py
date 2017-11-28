883b76f4b3e9594287cb6f7716a1402e9371a856
Erfan Noury <erfannoury@users.noreply.github.com>
2017-11-27 19:42:51 -0500

    Chang the Game class to Player class and refactor code

--- Game.py
+++ Game.py
@@ -1,39 +1,37 @@
 """
-Contains the Game class
+Contains the Player class
 """
 from rule_functions import *
+import phase2
 
 
-class Game:
+class Player:
     """
-    The Game class which contains the logic of the New Eleusis game.
+    The Player class which contains the logic of the New Eleusis game.
 
     Parameters
     ----------
-    board_state: list
+    cards: list
         A list of tuples, where the first item of the tuple is a card
         that has been accepted as legal in the sequence and the second tuple
         is a list (can be empty) which are the cards played after the last
         legal card which were illegal according to the dealer.
-    hypothesis_set: list
-        A list of lists, in which the inner lists contain a number of rules
-        that are disjuncted and the resulting rules from the inner lists are
-        then conjuncted to create the final rule.
     """
-    def __init__(self, board_state=None, hypothesis_set=None):
-        # Note the true rule gets defined later
-        self.board_state = board_state
-        self.hypothesis_set = hypothesis_set
+    def __init__(self, cards):
+        assert len(cards) == 3
+        self.board_state = []
+        for c in cards:
+            self.board_state.append((c, []))
+        self.applyAcceptedCard(cards[2])
 
-        # If they are not initialized, make them empty lists
-        if board_state is None:
-            self.board_state = []
-        if hypothesis_set is None:
-            self.hypothesis_set = []
+        self.hand = [generate_random_card() for i in range(14)]
+        self.hypothesis_set = []
+        self.turn = 0
 
     def rule(self):
         """
         This function returns the game rule to the user
+        TODO: This function should be removed
 
         Returns
         -----------
@@ -45,6 +43,7 @@ class Game:
     def setRule(self, rule_expr):
         """
         This function sets a given rule for a game
+        TODO: This function should be removed
 
         Parameters
         ----------
@@ -78,18 +77,11 @@ class Game:
             The next card to play
 
         """
-        #Get which turn we are on from how many cards have been played
-        turn = -3
-        for item in self.board_state:
-            #add one for the accepted card, and the number of rejected cards
-            turn += len(item[1]) + 1
-
-
         prev = self.board_state[-1][0]
         prev2 = self.board_state[-2][0]
 
         rule_tree = parse(combineListOfRules(self.hypothesis_set))
-        if turn % 2 == 0:
+        if self.turn % 2 == 0:
             for cur in hand:
                 if rule_tree.evaluate([prev2, prev, cur]):
                     return cur
@@ -208,48 +200,10 @@ class Game:
         """
         pass
 
-    def scientist(self, cards, hand, game_ended):
-        """
-        The function that plays a card and returns either a rule or the card played
-
-        Returns
-        -------
-        A rule or the card played
-
-        """
-
-        if len(self.hypothesis_set) == 0:
-            for card in cards:
-                self.board_state.append((card, []))
-            #We get three cards to start with, so make the initial hypothesis
-            self.hypothesis_set.append(getRulesForSequence(cards))
-        else:
-            #apply the cards played by the other players to our understanding of the game
-            for card in cards:
-                if is_card(card):
-                    if self.play(card):
-                        self.applyAcceptedCard(card)
-                    else:
-                        self.applyRejectedCard(card)
-
-        if game_ended:
-            return combineListOfRules(self.hypothesis_set)
-
-
-        chosen = self.chooseCard(hand)
-        if self.play(chosen):
-            self.applyAcceptedCard(chosen)
-        else:
-            self.applyRejectedCard(chosen)
-        self.simplifyRules()
-
-        #return combineListOfRules(self.hypothesis_set)
-        #TODO: return a rule if it hasn't changed in L amount of turns
-        return chosen
-
     def score(self):
         """
         This function returns the score for the game played so far
+        TODO: This function should no more be part of this class
 
         Returns
         -------
@@ -295,37 +249,50 @@ class Game:
             score += 15
         return score
 
-    def play(self, card):
+    def update_card_to_boardstate(self, card, result):
         """
-        Plays the card and returns whether or not it was valid.
-        Also adds it to the board state
+        Update your board state with card based on the result
 
         Parameters
         ----------
         card: str
-            The card that the player has played
+            The card that was played
+        result: bool
+            Whether the dealer accepted the card or not
+        """
+        self.turn += 1
+        previous = self.board_state[-1][0]
+        previous2 = self.board_state[-2][0]
+        if result:
+            self.board_state.append((card, []))
+            self.applyAcceptedCard(card)
+        else:
+            self.board_state[-1][1].append(card)
+            self.applyRejectedCard(card)
+
+    def play(self):
+        """
+        Either plays a card or returns a rule
 
         Returns
         -------
-        valid: bool
-            Whether the card played was legal or not
+        chosen: str
+            The next card to be played
+        OR
+        rule: str
+            The rule hypothesized so far
         """
-        previous = self.board_state[-1][0]
-        previous2 = self.board_state[-2][0]
-        valid = self.true_rule.evaluate([previous2, previous, card])
-        if isinstance(valid,str):
-            if valid == "True":
-                self.board_state.append((card, []))
-                valid = True
-            else:
-                self.board_state[-1][1].append(card)
-                valid = False
-        else:
-            if valid:
-                self.board_state.append((card, []))
-            else:
-                self.board_state[-1][1].append(card)
-        return valid
+
+        if phase2.game_ended:
+            return combineListOfRules(self.hypothesis_set)
+
+        # TODO: if we think the game should end
+        # phase2.game_ended = True
+        # return combineListOfRules(self.hypothesis_set)
+
+        chosen = self.chooseCard(hand)
+        self.hand.append(generate_random_card())
+        return chosen
 
     def getValidCards(self):
         """

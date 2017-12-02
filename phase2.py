c415aa798a5bc105e666f340a320ced76fdee846
Erfan Noury <erfan.noury@outlook.com>
2017-12-01 22:53:26 -0500

    Update phase2.py and remove the main from phase2

--- phase2.py
+++ phase2.py
@@ -1,14 +1,14 @@
-# Put your program name in place of program_name
-
-import Game
 from random import randint
-from new_eleusis import *
+from rule_functions import generate_random_card
 
 global game_ended
 game_ended = False
 
 
 class Adversary(object):
+    """
+    This is a random player as adversary
+    """
     def __init__(self):
         self.hand = [generate_random_card() for i in range(14)]
 
@@ -36,97 +36,3 @@ class Adversary(object):
             return rule[:-2] + ")"
         else:
             return self.hand[randint(0, len(self.hand) - 1)]
-
-
-if __name__ == '__main__':
-    # Set a rule for testing
-    rule = "if(is_royal(current), False)"
-    judge = Game.Scorer(rule)
-
-    cards = ["10H", "2C", "4S"]
-    tree = parse(rule)
-
-    player = Game.Player(cards)
-    adversary1 = Adversary()
-    adversary2 = Adversary()
-    adversary3 = Adversary()
-
-    # The score function needs to know if this was the player that ended the
-    # game
-    ended_player = 0
-
-    for round_num in range(14):
-        try:
-            # Player 1 plays
-            player_card_rule = player.play()
-            print("The player played", player_card_rule)
-            if is_card(player_card_rule):
-                # checking whether card played is correct or wrong
-                temp_cards = [cards[-2], cards[-1], player_card_rule]
-                result = tree.evaluate(tuple(temp_cards))
-                if result:
-                    del cards[0]
-                    cards.append(player_card_rule)
-                # player updating board state based on card played and result
-                player.update_card_to_boardstate(player_card_rule, result)
-            else:
-                ended_player = 0
-                raise Exception('')
-
-            # Adversary 1 plays
-            ad1_card_rule = adversary1.play()
-            print("Adversary 1 played", ad1_card_rule)
-            if is_card(ad1_card_rule):
-                temp_cards = [cards[-2], cards[-1], ad1_card_rule]
-                result = tree.evaluate(tuple(temp_cards))
-                if result:
-                    del cards[0]
-                    cards.append(ad1_card_rule)
-                player.update_card_to_boardstate(ad1_card_rule, result)
-            else:
-                ended_player = 1
-                raise Exception('')
-
-            # Adversary 2 plays
-            ad2_card_rule = adversary2.play()
-            print("Adversary 2 played", ad2_card_rule)
-            if is_card(ad2_card_rule):
-                temp_cards = [cards[-2], cards[-1], ad2_card_rule]
-                result = tree.evaluate(tuple(temp_cards))
-                if result:
-                    del cards[0]
-                    cards.append(ad2_card_rule)
-                player.update_card_to_boardstate(ad2_card_rule, result)
-            else:
-                ended_player = 2
-                raise Exception('')
-
-            # Adversary 3 plays
-            ad3_card_rule = adversary3.play()
-            print("Adversary 3 played", ad3_card_rule)
-            if is_card(ad3_card_rule):
-                temp_cards = [cards[-2], cards[-1], ad3_card_rule]
-                result = tree.evaluate(tuple(temp_cards))
-                if result:
-                    del cards[0]
-                    cards.append(ad3_card_rule)
-                player.update_card_to_boardstate(ad3_card_rule, result)
-            else:
-                ended_player = 3
-                raise Exception('')
-
-            print("The board state is:", player.boardState())
-
-        except:
-            print("The game ends!")
-            game_ended = True
-            break
-
-    # Everyone has to guess a rule
-    rule_player = player.play(True)
-    print("The rule guessed by the player was", rule_player)
-
-    # Check if the guessed rule is correct and print the score
-    is_player = (ended_player == 0)
-    the_score = judge.score(player)
-    print("The score for player was ", the_score)
